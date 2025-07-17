cd7948a2ddd29b932c7d8207e98b1df6d99fd764
emmastonecode <ighoe571@gmail.com>
2025-07-17 21:17:22 +0100

    fix: downgrade numpy for compatibility

--- app/main.py
+++ app/main.py
@@ -19,6 +19,8 @@ app.add_middleware(
         "http://localhost:8081",
         "http://localhost:19006",
         "http://127.0.0.1:8081",
+        "http://172.16.0.117:8000"
+
     ],
     allow_credentials=True,
     allow_methods=["*"],
